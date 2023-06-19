50d45f348495260f06aaf233c08ea731f67d1243
Dayane Lima <dayanelima.dev@gmail.com>
2023-06-19 16:10:13 -0300

    Renomiei o Ex 003B para Ex 004 e Resolvi o Ex 005

---
+++ Ex 005.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número inteiro e mostre na tela o
+# seu sucessor e antecessor.
+
+n = int(input('Digite um numero: '))
+a = n - 1
+s = n + 2
+
+print(f'O número {n}, tem como antecessor {a} e sucessor {s}')
