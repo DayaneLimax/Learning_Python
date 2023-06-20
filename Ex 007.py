4bdae8856099851e5550c62da879331e83b0e060
Dayane Lima <dayanelima.dev@gmail.com>
2023-06-20 19:04:05 -0300

    Alterado um erro

--- Ex 007.py
+++ Ex 007.py
@@ -3,6 +3,6 @@
 
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
-media = n1 + n2 / 2
+media = (n1 + n2) / 2
 
 print(f'A média entre {n1} e {n2} é: {media}')
