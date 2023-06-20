528bb08392298c4054a04c0a89e8b575a161cc07
Dayane Lima <dayanelima.dev@gmail.com>
2023-06-20 18:53:23 -0300

    Criei algoritmo que le um numero e retorna o seu dobro, triplo e raiz quadrada

---
+++ Ex 006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int (input('Digite um numero: '))
+
+print(f'O dobro de {n} é: {2 * n}')
+print(f'O triplo de {n} é: {3 * n}')
+print(f'A raiz quadrada de {n} é: {n ** (1/2):.2f}')
