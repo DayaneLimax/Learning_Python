297922c3d80c7a5761681d117faefda25ee92e6f
Dayane Lima <dayanelima.dev@gmail.com>
2023-06-20 19:01:17 -0300

    Algoritmo que lê duas notas e calcula sua média

---
+++ Ex 007.py
@@ -0,0 +1,8 @@
+# Desenvolva um algoritmo que leia duas notas de u.m aluno
+# calcule e mostre a sua média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = n1 + n2 / 2
+
+print(f'A média entre {n1} e {n2} é: {media}')
