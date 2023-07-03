1a9764200e847def05370a2ddea44f96f74c40fe
Dayane Lima <dayanelima.dev@gmail.com>
2023-07-03 15:49:16 -0300

    Ex conversor de temperatura

---
+++ EX 014 - Conversor de temperatura.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converta uma temperatura digitando em
+# graus Celsius e converta para graus Fahrenheit.
+
+c = float(input('Informe a temparura em c°: '))
+f = 9 * c / 5 + 32
+print(f'A temperatura de {c}c° corresponde a {f}f°!')
